c673439a7d868896541bc8d06e4f68809a3168c5
Mingyun KIM <mikim@e1z2r9p35.42.us.org>
2018-01-24 15:45:04 -0800

    normed

--- includes/assembler.h
+++ includes/assembler.h
@@ -6,7 +6,7 @@
 /*   By: apuel <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 14:09:54 by apuel             #+#    #+#             */
-/*   Updated: 2018/01/23 14:09:57 by apuel            ###   ########.fr       */
+/*   Updated: 2018/01/24 15:43:55 by mikim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 #include "op_ext.h"
 
 void		write_byteswapped(void *dst, void *src, size_t n);
-t_program	*the_assemble_everything_function(char *source);
 long		throw_error(char *string, long result);
 long		throw_verbose_error(char *format, long f0, long f1, long f2);
+void		create_binary(t_program *program, char *name, size_t size);
+
+t_program	*the_assemble_everything_function(char *source);
 
 #endif
--- srcs_asm/main.c
+++ srcs_asm/main.c
@@ -6,21 +6,19 @@
 /*   By: apuel <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 13:55:08 by apuel             #+#    #+#             */
-/*   Updated: 2018/01/24 14:51:29 by mikim            ###   ########.fr       */
+/*   Updated: 2018/01/24 15:42:17 by mikim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
 #include "assembler.h"
 
-long			throw_error(char *string, long result)
+long	throw_error(char *string, long result)
 {
 	ft_printf("[!] %s\n", string);
 	return (result);
 }
 
-long			throw_verbose_error(char *format, long f0, long f1, long f2)
+long	throw_verbose_error(char *format, long f0, long f1, long f2)
 {
 	ft_printf("[!] ");
 	ft_printf(format, f0, f1, f2);
@@ -28,66 +26,69 @@ long			throw_verbose_error(char *format, long f0, long f1, long f2)
 	return (-1);
 }
 
-unsigned char	*ft_readfile(char *path, size_t *size)
+char	*ft_readfile(char *path)
 {
-	unsigned char	*buffer;
-	int				fd;
+	char	*buffer;
+	size_t	size;
+	int		fd;
 
 	if ((fd = open(path, O_RDONLY, 0)) < 0)
 		return (NULL);
-	*size = lseek(fd, 0, SEEK_END);
-	if ((*size < 1) || !(buffer = ft_memalloc(*size)))
+	size = lseek(fd, 0, SEEK_END);
+	if ((size < 1) || !(buffer = ft_memalloc(size + 1)))
 		return (NULL);
 	lseek(fd, 0, SEEK_SET);
-	read(fd, buffer, *size);
+	read(fd, buffer, size);
 	close(fd);
 	return (buffer);
 }
 
-int				main(int argc, char **argv)
+void	create_binary(t_program *program, char *name, size_t size)
 {
-	char		*source;
-	char		*deleteme;
-	size_t		size;
+	char	*tmp;
+	int		fd;
+
+	if ((tmp = ft_strjoin(name, "or")))
+	{
+		tmp[size - 1] = 'c';
+		if ((fd = open(tmp, O_CREAT | O_TRUNC | O_WRONLY, 0666)) >= 0)
+		{
+			size = program->header.prog_size;
+			write_byteswapped(&program->header.prog_size, &size, 4);
+			write(fd, program, sizeof(t_program) + size);
+			close(fd);
+			ft_printf("Done!\n");
+		}
+		else
+			ft_printf("[!] Failed to create file, %s!\n", tmp);
+		free(tmp);
+	}
+	else
+		throw_error("Failed to allocate memory!", 0);
+	free(program);
+}
+
+int		main(int argc, char **argv)
+{
 	t_program	*program;
+	char		*source;
+	size_t		size;
 	int			i;
-	int			fd;
 
 	i = 0;
 	while (++i < argc)
 	{
-		size = ft_strlen(argv[i]);
-		if (!size || (argv[i][size - 1] != 's' &&
-				argv[i][size - 1] != 'S') || argv[i][size - 2] != '.')
+		if (!(size = ft_strlen(argv[i])) || (argv[i][size - 1] != 's' &&
+			argv[i][size - 1] != 'S') || argv[i][size - 2] != '.')
 		{
 			ft_printf("[!] %s has an invalid filetype!\n", argv[i]);
 			continue ;
 		}
 		ft_printf("Assembling %s...\n", argv[i]);
-		if ((source = (char *)ft_readfile(argv[i], &size)))
+		if ((source = ft_readfile(argv[i])))
 		{
 			if ((program = the_assemble_everything_function(source)))
-			{
-				size = ft_strlen(argv[i]);
-				if ((deleteme = ft_strjoin(argv[i], "or")))
-				{
-					deleteme[size - 1] = 'c';
-					if ((fd = open(deleteme, O_CREAT | O_TRUNC | O_WRONLY, 0666)) >= 0)
-					{
-						size = program->header.prog_size;
-						write_byteswapped(&program->header.prog_size, &size, 4);
-						write(fd, program, sizeof(t_program) + size);
-						close(fd);
-						ft_printf("Done!\n", argv[i]);
-					}
-					else
-						ft_printf("[!] Failed to create file, %s!\n", deleteme);
-					free(deleteme);
-				}
-				else
-					throw_error("Failed to allocate memory!", 0);
-				free(program);
-			}
+				create_binary(program, argv[i], size);
 			free(source);
 		}
 		else
